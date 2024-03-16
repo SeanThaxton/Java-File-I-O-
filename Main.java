4807d5417d92f504fd9bbc5cc8252eb079afcd03
KingOfNooks <seandthaxton1@gmail.com>
2024-03-16 16:18:36 -0400

    Adding some error handling to getting the user input file.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.BufferedWriter;
 
 /**
@@ -15,10 +19,19 @@ public class Main {
          System.out.println("Enter the path and the name, of  the file you want to reformat: ");
          Scanner theScan = new Scanner(System.in);
          String user = theScan.nextLine();
+         File userFile = new File(user);
+
+         if(userFile.exists() && !userFile.isDirectory()){
+               System.out.println("File exist in a different Dictionary");
+         }else{
+               System.out.println("File does not exist");
+         }
          theScan.close();
 
         //Parse each sentence of the file to be edit.
-         BufferedReader reader = new BufferedReader();
+        BufferedReader reader = new BufferedReader(new FileReader(userFile));
+        String txt = reader.readLine();
+        reader.close();
 
          BufferedWriter writer = new BufferedWriter();
 
