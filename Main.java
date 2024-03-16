ea41839ff590f65d324b938ff4d17b7ae23bc179
KingOfNooks <seandthaxton1@gmail.com>
2024-03-15 20:51:05 -0400

    Added the buffered Reader/Writer. Now time figure out how to use them in code.

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.BufferedWriter;
 
 /**
  * Main
@@ -10,11 +12,16 @@ public class Main {
         //Creates temp file to hold the reformated data, for the original.
 
         //Prompt user for the target file to be reformated
-        System.out.println("Enter the path and the name, of  the file you want to reformat: ");
-        Scanner theScan = new Scanner(System.in);
-        String user = theScan.nextLine();
+         System.out.println("Enter the path and the name, of  the file you want to reformat: ");
+         Scanner theScan = new Scanner(System.in);
+         String user = theScan.nextLine();
+         theScan.close();
 
         //Parse each sentence of the file to be edit.
+         BufferedReader reader = new BufferedReader();
+
+         BufferedWriter writer = new BufferedWriter();
+
         
      }
 }
