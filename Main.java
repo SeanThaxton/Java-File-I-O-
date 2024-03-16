c659425ca4b8b3ce6c2c0795d97c661da7a58856
KingOfNooks <seandthaxton1@gmail.com>
2024-03-15 20:05:33 -0400

    Real first commit since started working on the project

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+     public static void main(String[] args) {
+
+        //Creates temp file to hold the reformated data, for the original.
+
+        //Prompt user for the target file to be reformated
+        System.out.println("Enter the path and the name, of  the file you want to reformat: ");
+        Scanner theScan = new Scanner(System.in);
+        String user = theScan.nextLine();
+
+        //Parse each sentence of the file to be edit.
+        
+     }
+}
